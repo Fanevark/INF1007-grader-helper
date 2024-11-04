61de3260a6fae02f84319b826b08375d6c6a1968
fanevrk <faneva.rakotoarivony@polymtl.ca>
2024-11-04 11:01:47 -0500

    Better env vars handling

--- constants.py
+++ constants.py
@@ -4,21 +4,21 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv() 
 
-NUMERO_TP = "TP1"
-NUMERO_LAB = "L04"
-OUTPUT_FOLDER = "output"
+def load_env_variable(var_name: str) -> str: 
+    var = os.getenv(var_name)
+    if var is None: 
+        raise EnvironmentError(f"{var_name} is not set in .env file")
+    return var
 
-GRADING_FOLDER = "../correction"
+CODE_ASSIGNMENT = load_env_variable("CODE_ASSIGNMENT")
+SECTION_NUMBER = load_env_variable("NUMERO_SECTION")
+OUTPUT_FOLDER = f"./results/{CODE_ASSIGNMENT}-results"
 
-STUDENTS_CSV_FILENAME = "INF1007-L04.csv"
+GRADING_FOLDER = load_env_variable("GRADING_FOLDER")
 
-CORRECTOR_MAIL = os.getenv("CORRECTOR_MAIL")
-CORRECTOR_MAIL_USERNAME = os.getenv("CORRECTOR_MAIL_USERNAME")
-CORRECTOR_MAIL_PASSWORD = os.getenv("CORRECTOR_MAIL_PASSWORD")
+ASSIGNMENT_DATA_FOLDER = load_env_variable("ASSIGNMENT_DATA_FOLDER")
+STUDENTS_CSV_FILENAME = load_env_variable("STUDENTS_CSV_FILENAME")
 
-if CORRECTOR_MAIL is None:
-    raise EnvironmentError("CORRECTOR_MAIL is not set in .env file")
-if CORRECTOR_MAIL_USERNAME is None:
-    raise EnvironmentError("CORRECTOR_MAIL_USERNAME is not set in .env file")
-if CORRECTOR_MAIL_PASSWORD is None:
-    raise EnvironmentError("CORRECTOR_MAIL_PASSWORD is not set in .env file")
+CORRECTOR_MAIL = load_env_variable("CORRECTOR_MAIL")
+CORRECTOR_MAIL_USERNAME = load_env_variable("CORRECTOR_MAIL_USERNAME")
+CORRECTOR_MAIL_PASSWORD = load_env_variable("CORRECTOR_MAIL_PASSWORD")
